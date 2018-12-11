40e61275a072591c4ef2abc40e98b3140430735f
Matthew Chuong <mstchuong@yahoo.com>
2018-12-11 15:29:52 -0500

    Add files via upload

---
+++ src/CardComponent/card.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Card extends Component {
+    render() {
+        return(        
+        <div className="Card">
+            Card Works
+        </div>
+        );
+    }
+}
+
+export default Card;
